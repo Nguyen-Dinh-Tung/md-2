43ed360143e8b53cd1ed58fdcfee974523069dcf
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-07-04 11:20:27 +0700

    update

---
+++ typescript/looparowfunc/my.js
@@ -0,0 +1,13 @@
+// function print() {
+//   console.log("for(;;;)");
+//   for (let i = 0; i < 10; i += 2) {
+//     console.log(i + "");
+//   }
+// }
+// print();
+// for (let i in [1, 2, 3, "ád"]) {
+//   console.log(i);
+// }
+// for (let i of [1, 2, "a", " next"]) {
+//   console.log(i);
+// }
