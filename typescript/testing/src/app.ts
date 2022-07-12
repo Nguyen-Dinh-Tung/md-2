6b481802c83189dda84839db16a72b89ecc7731b
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-07-12 20:04:33 +0700

    update

--- typescript/testing/src/app.ts
+++ typescript/testing/src/app.ts
@@ -1,5 +1,5 @@
-export function filterByTerm(arr: any, pattern: string) {
-  return arr.filter( (item: any) => {
-      return item.url.match(pattern);
-  });
+export class Calculator {
+  public static add(x : number , y: number) : number{
+    return x + y
+  }
 }
