a0f8ca9d3c7e69692ea39f56509fc55dac28a7f7
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-07-21 08:55:20 +0700

    update

---
+++ typescript/debug/exercise/main.ts
@@ -0,0 +1,24 @@
+function calc(number1 , number2 , operator){
+  let result ;
+  if(operator == '+'){
+    result = number1 + number2
+  }else if(operator == '-'){
+    result = number1 - number2
+  }if(operator == '/'){
+    result = number1 / number2
+  }if(operator == '*'){
+    result = number1 * number2
+  }else{
+  throw RangeError ('Operator must be + - * /')
+  }
+  return result
+}
+
+try{
+  console.log(calc(3,2,'%'))
+}
+catch(e){
+  if(e instanceof RangeError){
+    console.log(e.message)
+  }
+}
